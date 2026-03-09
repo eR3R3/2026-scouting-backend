4a038a424c3a0f4cf05b0e2e214e89899aafaeaf
LCsiu <yooyooyoojun@163.com>
2026-03-09 16:43:10 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
   app.enableCors({
     origin: true, // Allow all origins for Vercel
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders: ['Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.init();
